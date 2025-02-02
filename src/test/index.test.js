75f1fd5b86664fef69d3ae06698631dc0d01e47a
Danilo A. Silva <danilo.official22@hotmail.com>
2025-02-02 20:57:57 -0300

    creating tests

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,14 +1,14 @@
-// const request = require('supertest')
-// const app = require('../') // Importa a aplicação Express
+const request = require('supertest')
+const app = require('../') // Importa a aplicação Express
 
-// describe('GET /', () => {
-//     it('should return Hello World!', async () => {
-//         const response = await request(app).get('/')
+describe('GET /', () => {
+    it('should return Hello World!', async () => {
+        const response = await request(app).get('/')
 
-//         // Verifica se o status da resposta é 200 (OK)
-//         expect(response.status).toBe(200)
+        // Verifica se o status da resposta é 200 (OK)
+        expect(response.status).toBe(200)
 
-//         // Verifica se o corpo da resposta é 'Hello World!'
-//         expect(response.text).toBe('Hello World!')
-//     })
-// })
+        // Verifica se o corpo da resposta é 'Hello World!'
+        expect(response.text).toBe('Hello World!')
+    })
+})
